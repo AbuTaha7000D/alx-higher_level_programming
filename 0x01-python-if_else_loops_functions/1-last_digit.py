945dd1efeecd20f2dd16b211acc07c551ef7f397
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-10-31 23:44:45 +0200

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,15 @@ number = random.randint(-10000, 10000)
 number2 = number % 10
 if number >= 0:
     if number2 > 5:
-        print("Last digit of", number, "is", number2, "is greater than 5")
+        print("Last digit of", number, "is", number2, "and is greater than 5")
     elif number2 == 0:
-        print("Last digit of", number, "is", number2, "is 0")
+        print("Last digit of", number, "is", number2, "and is 0")
     else:
-        print("Last digit of", number, "is", number2, "is less than 6 and not 0")
+        print("Last digit of", number, "is", number2, "and is less than 6 and not 0")
 if number < 0:
     if number2 == 0:
-        print("Last digit of", number, "is", number2, "is 0")
+        print("Last digit of", number, "is", number2, "and is 0")
     else:
-        print("Last digit of", number, "is", number2 - 10, "is less than 6 and not 0")
+        print(
+            "Last digit of", number, "is", number2 - 10, "and is less than 6 and not 0"
+        )
