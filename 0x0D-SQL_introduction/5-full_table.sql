d2fe17dcce6ccc57ca8eaa836dfa705b65757726
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2024-03-28 00:24:38 +0200

    Q5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
